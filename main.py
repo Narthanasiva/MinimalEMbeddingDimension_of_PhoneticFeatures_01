67025b28b7da656dfe5bd87fcbcb2275726c059b
Narthanasiva <narththu99@gmail.com>
2025-11-11 20:57:03 +0000

    update in the code of creating output files for every dataset

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 # main.py
 from src.utils import make_dataset_output_folders
+import argparse
 
 if __name__ == "__main__":
-    dataset_name = "TIMIT"
+    parser = argparse.ArgumentParser(description="Create output folder structure for a dataset.")
+    parser.add_argument("dataset_name", type=str, help="Name of the dataset for which to create output folders.")
+    args = parser.parse_args()
+
+    dataset_name = args.dataset_name
     make_dataset_output_folders(dataset_name)
     print(f"Output folder structure created for {dataset_name}")
